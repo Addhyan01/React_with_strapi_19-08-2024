b0cf8af9048ba35ff7f8e97f1ef43426be7c86f4
Addhyan01 <addhyankumar@gmail.com>
2024-08-19 02:59:24 +0530

    add Roues using diffrent way like we create a routes.js and pass the data in gthis and call by map function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { routes } from './routes/Route'
+
 
 export const App = () => { //this is fat Arror  ==> ES6 ===> 2015
-  return (
-    <div>App</div>
-  )
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                {
+
+                    routesgit .map((currentValue, index, arr) => {
+                        return ( <Route key={index} path={currentValue.path} element={ currentValue.element} />)
+                    })
+
+            }
+
+
+            </Routes>
+        </BrowserRouter>
+    )
 }
---
+++ src/routes/Route.js
@@ -0,0 +1,21 @@
+
+
+import Contact from "../pages/Contact";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+
+
+export const  routes = [
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/contact",
+        element: <Contact />,
+    },
+];
